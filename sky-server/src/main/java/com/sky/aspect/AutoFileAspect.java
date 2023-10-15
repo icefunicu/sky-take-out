6cc145e96082e2edcf27a451d8d66cd9d59e42ac
icefun <im@icefun.icu>
2023-10-15 18:24:20 +0800

    添加自定义注解实现公共字段填充

---
+++ sky-server/src/main/java/com/sky/aspect/AutoFileAspect.java
@@ -0,0 +1,85 @@
+package com.sky.aspect;
+
+import com.sky.annotation.AutoFile;
+import com.sky.constant.AutoFillConstant;
+import com.sky.context.BaseContext;
+import com.sky.enumeration.OperationType;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MemberSignature;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+/*
+* 自定义切面，用于公共字段填充逻辑
+*
+* */
+@Aspect
+@Component
+@Slf4j
+public class AutoFileAspect {
+
+    /*
+    * 切入点
+    * */
+    @Pointcut("execution(* com.sky.mapper.*.*(..)) && @annotation(com.sky.annotation.AutoFile)")
+    public void autoFilePointCut(){
+
+    }
+
+
+    /*
+    * 前置通知
+    * */
+    @Before("autoFilePointCut()")
+    public void autoFile(JoinPoint joinPoint) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        log.info("开始公共字段填充");
+        //获取到当前被拦截的方法上的数据库操作类型
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();//方法签名对象
+        AutoFile autoFile = signature.getMethod().getAnnotation(AutoFile.class);//获得方法上的注解对象
+        OperationType operationType = autoFile.value();//获得数据库操作类型
+        //获取到当前被拦截的方法的参数--实体对象
+        Object[] args = joinPoint.getArgs();
+        if (args==null||args.length==0){
+            return;
+        }
+        Object arg = args[0];
+        //准备赋值的数据
+        LocalDateTime now = LocalDateTime.now();
+        Long currentId = BaseContext.getCurrentId();
+        //根据不同的操作类型，为对应的属性通过反射来赋值
+        if (operationType == OperationType.INSERT){
+            //为四个公共字段赋值
+            Method setCreateTime = arg.getClass().getDeclaredMethod(AutoFillConstant.SET_CREATE_TIME, LocalDateTime.class);
+            Method setCreateUser = arg.getClass().getDeclaredMethod(AutoFillConstant.SET_CREATE_USER, Long.class);
+            Method setUpdateTime = arg.getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_TIME, LocalDateTime.class);
+            Method setUpdateUser = arg.getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_USER, Long.class);
+            //通过反射为对象属性赋值
+            setCreateTime.invoke(arg,now);
+            setCreateUser.invoke(arg,currentId);
+            setUpdateTime.invoke(arg,now);
+            setUpdateUser.invoke(arg,currentId);
+        }else if(operationType==OperationType.UPDATE){
+            //为两个公共字段赋值
+            Method setUpdateTime = arg.getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_TIME, LocalDateTime.class);
+            Method setUpdateUser = arg.getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_USER, Long.class);
+            setUpdateTime.invoke(arg,now);
+            setUpdateUser.invoke(arg,currentId);
+        }
+
+
+
+    }
+
+
+
+}
--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,8 +1,10 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.annotation.AutoFile;
 import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -22,6 +24,7 @@ public interface EmployeeMapper {
     /**
      * 插入员工数据
      */
+    @AutoFile(value = OperationType.INSERT)
     @Insert("INSERT INTO employee (name, username, password, phone, sex, id_number, create_time, update_time, create_user, update_user,status) " +
             "VALUES " +
             "(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{createTime},#{updateTime},#{createUser},#{updateUser},#{status})" )
@@ -35,6 +38,7 @@ public interface EmployeeMapper {
      * 根据主键动态修改属性
      * @param employee
      */
+    @AutoFile(value = OperationType.UPDATE)
     void update(Employee employee);
     /**
      * 根据id查询员工信息
