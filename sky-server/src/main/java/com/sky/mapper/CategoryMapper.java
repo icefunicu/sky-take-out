a0b43e3430eb4b8c1d7e9a3d394e527aa8bef5ba
icefun <im@icefun.icu>
2023-11-02 21:07:45 +0800

    新增菜品以及文件上传功能

--- sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
+++ sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
@@ -1,10 +1,9 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
-<<<<<<< HEAD
+
 import com.sky.annotation.AutoFile;
-=======
->>>>>>> 4dc56e4a529fc371177dba168fabd0fc2e2e27e6
+
 import com.sky.enumeration.OperationType;
 import com.sky.dto.CategoryPageQueryDTO;
 import com.sky.entity.Category;
@@ -20,10 +19,8 @@ public interface CategoryMapper {
      * 插入数据
      * @param category
      */
-<<<<<<< HEAD
+
     @AutoFile(value = OperationType.INSERT)
-=======
->>>>>>> 4dc56e4a529fc371177dba168fabd0fc2e2e27e6
     @Insert("insert into category(type, name, sort, status, create_time, update_time, create_user, update_user)" +
             " VALUES" +
             " (#{type}, #{name}, #{sort}, #{status}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
@@ -47,10 +44,9 @@ public interface CategoryMapper {
      * 根据id修改分类
      * @param category
      */
-<<<<<<< HEAD
+
     @AutoFile(value = OperationType.UPDATE)
-=======
->>>>>>> 4dc56e4a529fc371177dba168fabd0fc2e2e27e6
+
     void update(Category category);
 
     /**
